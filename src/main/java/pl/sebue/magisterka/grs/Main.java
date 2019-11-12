4b5f2942b16fa8c72353f414898e580c9954adee
sebue <sebastian.jasinski@sebue.pl>
2019-11-12 21:28:18 +0100

    Initial commit

---
+++ src/main/java/pl/sebue/magisterka/grs/Main.java
@@ -0,0 +1,7 @@
+package pl.sebue.magisterka.grs;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/pl/sebue/magisterka/grs/model/Game.java
@@ -0,0 +1,18 @@
+package pl.sebue.magisterka.grs.model;
+
+import java.util.Date;
+
+public class Game {
+    private String name;
+    private Date releaseDate;
+    private int requiredAge;
+    private boolean hasDemoVersion;
+    private int dlcCount;
+    private int metacriticScore;
+    private boolean isControllerSupported;
+    private int recommendationCount;
+    private int achievementCount;
+    private float initialPrice;
+    private float finalPrice;
+    private int ownerCount;
+}
